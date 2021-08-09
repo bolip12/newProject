fc607786d75bd4de9db32183da16b5444eff79de
bolip12 <alifsasetyaputra@gmail.com>
2021-08-09 16:49:28 +0700

    20210809

--- screen/UserNav/HomeScreen.js
+++ screen/UserNav/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Alert, StyleSheet, ScrollView } from 'react-native';
 import { Provider as PaperProvider, Appbar, List, Title, Surface, Text, Divider, IconButton } from 'react-native-paper';
+import { showMessage } from "react-native-flash-message";
 
 import supabase from '../../config/supabase';
 import Theme from '../../config/Theme';
@@ -35,6 +36,12 @@ class HomeScreen extends Component {
           type: 'LOGIN',
           payload: { isLogin:false }
       });
+
+      showMessage({
+        message: "Berhasil Logout",
+        type: 'success',
+        icon: 'success',
+      });
   }
 
   render() {
@@ -42,43 +49,79 @@ class HomeScreen extends Component {
         <PaperProvider theme={Theme}>
           <Appbar.Header>
             <Appbar.Content title="Home" />
-            <Appbar.Action icon="bell-ring-outline" onPress={() => this.props.navigation.navigate('PesanScreen')} />
+            <Appbar.Action icon="power" onPress={() => this.onLogout('')} />
+            {/*<Appbar.Action icon="bell-ring-outline" onPress={() => this.props.navigation.navigate('PesanScreen')} />*/}
           </Appbar.Header>
 
           <ScrollView style={styleApp.ScrollView}>
-          <View style={{alignSelf:'center', margin:10}}>
-            <Title>Selamat Datang</Title>
-          </View>
-          <Divider/>
 
 
-          <View style={{ flexDirection:'row', justifyContent: 'space-around'}}> 
-              <IconButton
-                icon="account"
-                size={30}
-                onPress={() => console.log('Pressed')}
-              />     
+            <View style={{ flexDirection:'row', justifyContent: 'space-evenly', marginTop:20}}> 
+              
+              <View>
+                <IconButton
+                  icon="account"
+                  size={40}
+                  color='white'
+                  style={{ backgroundColor: 'green'}}
+                  onPress={() => this.props.navigation.navigate('Setting', { screen: 'ProfilScreen'})}
+                />
+                <Text style={{ alignSelf: 'center', fontSize:15 }} >Profil</Text>
+              </View>
 
-              <IconButton
-                icon="account"
-                size={30}
-                onPress={() => console.log('Pressed')}
-              />      
+              <View>
+                <IconButton
+                  icon="format-list-numbered-rtl"
+                  size={40}
+                  onPress={() => console.log('Pressed')}
+                  color='white'
+                  style={{ backgroundColor: 'green' }}
+                /> 
+                <Text style={{ alignSelf: 'center', fontSize:15 }} >Tabungan</Text>  
+                <Text style={{ alignSelf: 'center', fontSize:15 }} >Qurban</Text>     
+              </View>
 
-              <IconButton
-                icon="account"
-                size={30}
-                onPress={() => console.log('Pressed')}
-              />              
+              <View>
+                <IconButton
+                  icon="history"
+                  size={40}
+                  onPress={() => console.log('Pressed')}
+                  color='white'
+                  style={{ backgroundColor: 'green'}}
+                />   
+                <Text style={{ alignSelf: 'center', fontSize:15 }} >Riwayat</Text>
+                <Text style={{ alignSelf: 'center', fontSize:15 }} >Transaksi</Text>    
+              </View>  
 
-              <IconButton
-                icon="account"
-                size={30}
-                onPress={() => console.log('Pressed')}
-              />      
-          </View>
+              
+               
+            </View>
 
-            
+            <View style={{ flexDirection:'row', marginTop: 25, marginHorizontal:50}}> 
+
+              <View>
+                <IconButton
+                  icon="message-alert-outline"
+                  size={40}
+                  onPress={() => this.props.navigation.navigate('PesanScreen')}
+                  color='white'
+                  style={{ backgroundColor: 'green'}}
+                />    
+                <Text style={{ alignSelf: 'center', fontSize:15 }} >Pesan</Text>    
+              </View>
+
+              <View style={{ marginLeft:45 }}> 
+                <IconButton
+                  icon="account-group"
+                  size={40}
+                  onPress={() => this.props.navigation.navigate('Anggota', { screen: 'AnggotaListScreen'})}
+                  color='white'
+                  style={{ backgroundColor: 'green' }}
+                />  
+                <Text style={{ alignSelf: 'center', fontSize:15 }} >Anggota</Text>               
+              </View>
+
+            </View>
 
           </ScrollView>
 
